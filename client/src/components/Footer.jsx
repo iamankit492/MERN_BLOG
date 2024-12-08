e0f72d79986b14eb7c6a9ca443df63b3d2a59be4
ankit-492 <chauhanankit492@gmial.com>
2024-12-08 06:23:33 +0530

    third

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -41,7 +41,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://www.github.com/iamankit492'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
